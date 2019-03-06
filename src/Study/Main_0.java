110aeb726c41f1f0ea9b729d96e3fd6843b2eb47
songz1 <21pori30@gmail.com>
2019-03-06 14:03:58 +0900

    add main_0

--- src/Study/Main_0.java
+++ src/Study/Main_0.java
@@ -1,8 +1,23 @@
 /*
-송지은_점수의 최댓값 찾기(재귀)
+송지은_최대값 찾기(재귀)
 */
 
 package Study;
 
 public class Main_0 {
+    public static void main(String[] args){
+        int[] scores = {76, 35, 98, 64, 87, 54, 100};
+
+        int max = maxScore(scores, 0, scores.length);
+        System.out.println(max);
+    }
+
+    public static int maxScore(int[] scores, int start, int end){
+        if(start == end)
+            return 0;
+        return Math.max(scores[start], maxScore(scores, start+1, end));
+    }
+
 }
+
+
