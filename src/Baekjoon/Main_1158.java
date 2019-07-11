7542aec78e5c22acfc7544cef5171dd5ff9e23a1
songz1 <21pori30@gmail.com>
2019-07-12 01:24:33 +0900

    add Main_1158

--- src/Baekjoon/Main_1158.java
+++ src/Baekjoon/Main_1158.java
@@ -13,20 +13,19 @@ public class Main_1158 {
 		int n = input.nextInt();
 		int k = input.nextInt();
 
-		ArrayList<Integer> list = new ArrayList<>();
-		ArrayList<Integer> result = new ArrayList<>();
-
+		ArrayList list = new ArrayList();
 		for (int i = 1; i <= n; i++) {
 			list.add(i);
 		}
 
 		int index = 0;
+		ArrayList result = new ArrayList();
 		while (list.size() > 0) {
-			index = (index + k - 1) % list.size();
+			index = (k + index - 1) % list.size();
 			result.add(list.get(index));
 			list.remove(index);
 		}
 
-		System.out.print(result.toString().replace('[', '<').replace(']', '>'));
+		System.out.println(result.toString().replace('[', '<').replace(']', '>'));
 	}
 }
