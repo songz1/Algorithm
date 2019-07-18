b05e657d7466edcd23d102784ab67aba0f3e0551
songz1 <21pori30@gmail.com>
2019-07-18 17:56:56 +0900

    add Main_9020

---
+++ src/Baekjoon/Main_9020.java
@@ -0,0 +1,15 @@
+/*
+송지은_백준 알고리즘 9020번
+*/
+
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_9020 {
+	public static void main(String[] args){
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
