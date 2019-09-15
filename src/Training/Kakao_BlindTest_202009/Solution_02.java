07d2005e03f48fcd0b84782d948127e654bb51e2
songz1 <21pori30@gmail.com>
2019-09-15 20:00:16 +0900

    add kakao 2020 02

---
+++ src/Training/Kakao_BlindTest_202009/Solution_02.java
@@ -0,0 +1,15 @@
+/*
+송지은_카카오 블라인드 코딩테스트_2번 균형잡힌 괄호 문자열
+*/
+
+package Training.Kakao_BlindTest_202009;
+
+public class Solution_02 {
+	public static void main(String[] args){
+
+	}
+
+	public static String solution(String p) {
+		return "";
+	}
+}
