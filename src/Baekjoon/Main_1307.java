16dbf305ea60a17616b3eb66e1c6a0491fef2f05
songz1 <21pori30@gmail.com>
2019-11-25 16:02:01 +0900

    add Main_1307 실패

--- src/Baekjoon/Main_1307.java
+++ src/Baekjoon/Main_1307.java
@@ -13,39 +13,10 @@ public class Main_1307 {
 		int n = Integer.parseInt(bufferedReader.readLine());
 		int[][] m = new int[n][n];
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				m[i][j] = 0;
-			}
-		}
+		reset(m, n);
 
 		if (n % 2 != 0) {
-			int a = n - 1;
-			int b = n / 2;
-			m[a][b] = 1;
-
-			for (int i = 2; i <= n * n; i++) {
-				a = a + 1;
-				b = b + 1;
-
-				if (a >= n && b >= n) {
-					a = a - 2;
-					b = b - 1;
-					m[a][b] = i;
-				} else if (a >= n) {
-					a = 0;
-					m[a][b] = i;
-				} else if (b >= n) {
-					b = 0;
-					m[a][b] = i;
-				} else if (m[a][b] != 0) {
-					a = a - 2;
-					b = b - 1;
-					m[a][b] = i;
-				} else {
-					m[a][b] = i;
-				}
-			}
+			oddNumber(m, n, 1);
 		} else {
 			if (n % 4 == 0) {
 				int value = 1;
@@ -81,8 +52,52 @@ public class Main_1307 {
 						}
 					}
 				}
-			}
+			} else if (n % 4 == 2) {
+				int range = n / 2;
+				int[][] section = new int[range][range];
 
+				reset(section, range);
+				oddNumber(section, range, 1);
+				for (int i = 0; i < range; i++) {
+					for (int j = 0; j < range; j++) {
+						m[i][j] = section[i][j];
+					}
+				}
+
+				reset(section, range);
+				oddNumber(section, range, range * range + 1);
+				for (int i = range; i < range * 2; i++) {
+					for (int j = range; j < range * 2; j++) {
+						m[i][j] = section[i - range][j - range];
+					}
+				}
+
+				reset(section, range);
+				oddNumber(section, range, range * range * 2 + 1);
+				for (int i = range; i < range * 2; i++) {
+					for (int j = 0; j < range; j++) {
+						m[i][j] = section[i - range][j];
+					}
+				}
+
+				reset(section, range);
+				oddNumber(section, range, range * range * 3 + 1);
+
+				swap(m, section, range / 2, range / 2);
+				for (int i = 0; i < range; i++) {
+					for (int j = 0; j < range; j++) {
+						if (j != range / 2) {
+							swap(m, section, i, j);
+						}
+					}
+				}
+
+				for (int i = 0; i < range; i++) {
+					for (int j = range; j < range * 2; j++) {
+						m[i][j] = section[i][j - range];
+					}
+				}
+			}
 		}
 
 		for (int i = 0; i < n; i++) {
@@ -90,6 +105,49 @@ public class Main_1307 {
 				System.out.print(m[i][j] + " ");
 			}
 			System.out.println();
+		}
+	}
+
+	static void reset(int[][] m, int n) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				m[i][j] = 0;
+			}
+		}
+	}
+
+	static void swap(int[][] a, int[][] b, int row, int hor) {
+		int temp = b[row][hor];
+		b[row][hor] = a[row][hor];
+		a[row][hor] = temp;
+	}
+
+	static void oddNumber(int[][] m, int n, int start) {
+		int a = n - 1;
+		int b = n / 2;
+		m[a][b] = start++;
+
+		for (int i = 2; i <= n * n; i++) {
+			a = a + 1;
+			b = b + 1;
+
+			if (a >= n && b >= n) {
+				a = a - 2;
+				b = b - 1;
+				m[a][b] = start++;
+			} else if (a >= n) {
+				a = 0;
+				m[a][b] = start++;
+			} else if (b >= n) {
+				b = 0;
+				m[a][b] = start++;
+			} else if (m[a][b] != 0) {
+				a = a - 2;
+				b = b - 1;
+				m[a][b] = start++;
+			} else {
+				m[a][b] = start++;
+			}
 		}
 	}
 }
