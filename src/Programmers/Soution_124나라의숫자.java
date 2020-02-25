8ccee93596a8a1bcd1ec594efd241d820f1a64ef
songz1 <21pori30@gmail.com>
2020-02-25 16:22:50 +0900

    add 124 나라의 숫자

---
+++ src/Programmers/Soution_124나라의숫자.java
@@ -0,0 +1,28 @@
+/*
+송지은_프로그래머스 알고리즘 124 나라의 숫자
+*/
+
+package Programmers;
+
+public class Soution_124나라의숫자 {
+	public static void main(String[] args) {
+		for (int i = 10; i < 21; i++) {
+			System.out.println("**************i=" + i);
+			System.out.println("ans= " + solution(i));
+			System.out.println();
+		}
+	}
+
+	public static String solution(int n) {
+		String answer = "";
+		int remainder;
+
+		while (n > 0) {
+			remainder = (n % 3 == 0 ? 4 : n % 3);
+			n = (remainder == 4 ? n / 3 - 1 : n / 3);
+			answer = remainder + answer;
+		}
+
+		return answer;
+	}
+}
