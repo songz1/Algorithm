f54718d78ebca47173b78ef851392ffbef727c55
songz1 <21pori30@gmail.com>
2020-03-05 10:40:22 +0900

    add 탑

---
+++ src/Programmers/Solution_탑.java
@@ -0,0 +1,27 @@
+/*
+송지은_프로그래머스 탑
+*/
+
+package Programmers;
+
+public class Solution_탑 {
+	public static void main(String[] args) {
+
+	}
+
+	public static int[] solution(int[] heights) {
+		int[] answer = new int[heights.length];
+		answer[0] = 0;
+
+		for (int i = heights.length - 1; i > 0; i--) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (heights[i] < heights[j]) {
+					answer[i] = j + 1;
+					break;
+				}
+			}
+		}
+
+		return answer;
+	}
+}
