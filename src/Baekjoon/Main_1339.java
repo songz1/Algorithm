8c7bf20d84ab33e1a64096b1a8eb273b5f20dc62
songz1 <21pori30@gmail.com>
2020-03-10 16:28:57 +0900

    add 1339

---
+++ src/Baekjoon/Main_1339.java
@@ -0,0 +1,53 @@
+/*
+송지은_백준 알고리즘 1339번
+
+num 배열 크기 10, 11로 하면 런타임 에러남. 왜?
+*/
+
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main_1339 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bufferedReader.readLine());
+
+		HashMap<Character, Integer> alphabet = new HashMap<>();
+		int[] num = new int[100];
+		int idx = 0;
+
+		for (int i = 0; i < N; i++) {
+			String str = bufferedReader.readLine();
+
+			int t = 1;
+			for (int j = str.length() - 1; j >= 0; j--) {
+				char c = str.charAt(j);
+
+				if (alphabet.containsKey(c)) {
+					num[alphabet.get(c)] += t;
+				} else {
+					num[idx] = t;
+					alphabet.put(c, idx);
+				}
+
+				idx++;
+				t = t * 10;
+			}
+		}
+
+		Arrays.sort(num);
+
+		int result = 0;
+		int count = 9;
+		for (int i = num.length - 1; i >= 0; i--) {
+			result += num[i] * count;
+			count--;
+		}
+
+		System.out.print(result);
+	}
+}
