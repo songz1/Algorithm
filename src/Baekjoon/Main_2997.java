045953f4405dde50dbeccb65b069c504ee043bb1
songz1 <21pori30@gmail.com>
2020-04-03 11:32:14 +0900

    2997 네 번째 수

---
+++ src/Baekjoon/Main_2997.java
@@ -0,0 +1,39 @@
+/*
+송지은_백준 알고리즘 2997번
+*/
+
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_2997 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int a, b;
+        a = arr[1] - arr[0];
+        b = arr[2] - arr[1];
+
+        if (a == b) {
+            System.out.print(arr[2] + a);
+            return;
+        } else if (a > b) {
+            System.out.print(arr[0] + b);
+            return;
+        } else {
+            System.out.print(arr[1] + a);
+            return;
+        }
+    }
+}
